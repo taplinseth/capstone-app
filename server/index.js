f0df6d71df3a6e9d3ec525c56ca88ede03bc6af2
taplinseth <taplinseth@gmail.com>
2022-03-03 12:55:01 -0600

    file name

---
+++ server/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var index = express();
+
+index.use(logger('dev'));
+index.use(express.json());
+index.use(express.urlencoded({ extended: false }));
+index.use(cookieParser());
+index.use(express.static(path.join(__dirname, 'public')));
+
+index.use('/', indexRouter);
+index.use('/users', usersRouter);
+
+module.exports = index;
